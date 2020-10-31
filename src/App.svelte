d01bb6dc9fd814d4ffe90f065018bb4d4c846238
Birgit Pohl <BirgitPohl@users.noreply.github.com>
2020-10-31 11:23:56 +0100

    Suuuuuper init

---
+++ src/App.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import { waitLocale, t, isLoading } from 'svelte-i18n'
+  	export async function preload() {
+		return waitLocale('en');
+	  }
+</script>
+
+{#if $isLoading}
+  Please wait...
+{:else}
+	<main>
+		<h1>Hello i18n!</h1>
+		{$t('website_under_construction')}
+	</main>
+{/if}
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
