c29fc86a6302df98ac1cf9bd43c287c0e348f2eb
Birgit Pohl <BirgitPohl@users.noreply.github.com>
2020-11-06 20:10:55 +0100

    Typescript doesn't work?

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import App from './App.svelte';
 import './services/i18n';
 
+// src/index.js
+import pkg from './package.json';
+console.log(`running version ${pkg.version}`);
+
 const app = new App({
 	target: document.body,
 	props: {
